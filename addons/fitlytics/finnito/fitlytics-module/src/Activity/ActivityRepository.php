8a72b10f81ee88fdeaf8d2b083981ff525f39f56
Finn Le Sueur <finn@lesueur.nz>
2021-05-02 22:01:40 +1200

    Adds some activity repo queries for maintainability

--- addons/fitlytics/finnito/fitlytics-module/src/Activity/ActivityRepository.php
+++ addons/fitlytics/finnito/fitlytics-module/src/Activity/ActivityRepository.php
@@ -21,5 +21,74 @@ class ActivityRepository extends EntryRepository implements ActivityRepositoryIn
     public function __construct(ActivityModel $model)
     {
         $this->model = $model;
+        $this->now = \Carbon\Carbon::now("UTC");
+        $this->offset = \Carbon\Carbon::createFromTimestamp(0, config('app.timezone'))->getTimezone()->toOffsetName();
+    }
+
+    public function weekBoundaries()
+    {
+        $now = \Carbon\Carbon::now("UTC");
+        // dd($now->startOfWeek());
+        return [
+            $now->startOfWeek()->format("M jS"),
+            $now->endOfWeek()->format("M jS"),
+        ];
+    }
+
+    public function currentWeekStatistics()
+    {
+        $now = \Carbon\Carbon::now("UTC");
+        $offset = \Carbon\Carbon::createFromTimestamp(0, config('app.timezone'))->getTimezone()->toOffsetName();
+        return $this->model->query()
+            ->selectRaw("SUM(distance) AS distance, SUM(total_elevation_gain) as elevation, SUM(moving_time) as moving_time")
+            ->whereRaw(
+                "CONVERT_TZ(STR_TO_DATE(start_date, '%Y-%m-%dT%H:%i:%sZ'), '+00:00','"
+                . $offset
+                . "') BETWEEN '"
+                . $now->startOfWeek()->format("Y-m-d H:i:s")
+                . "' AND '"
+                . $now->endOfWeek()->format("Y-m-d H:i:s")
+                . "'"
+            )
+            ->first();
+    }
+
+    public function currentWeekStatisticsByType()
+    {
+        $now = \Carbon\Carbon::now("UTC");
+        $offset = \Carbon\Carbon::createFromTimestamp(0, config('app.timezone'))->getTimezone()->toOffsetName();
+        return $this->model->query()
+            ->selectRaw("type, SUM(distance) AS distance, SUM(total_elevation_gain) as elevation, SUM(moving_time) as moving_time")
+            ->whereRaw(
+                "CONVERT_TZ(STR_TO_DATE(start_date, '%Y-%m-%dT%H:%i:%sZ'), '+00:00','"
+                . $offset
+                . "') BETWEEN '"
+                . $now->startOfWeek()->format("Y-m-d H:i:s")
+                . "' AND '"
+                . $now->endOfWeek()->format("Y-m-d H:i:s")
+                . "'"
+            )
+            ->groupBy("type")
+            ->orderBy("type", "asc")
+            ->get();
+    }
+
+    public function thisWeek()
+    {
+        $now = \Carbon\Carbon::now("UTC");
+        $offset = \Carbon\Carbon::createFromTimestamp(0, config('app.timezone'))->getTimezone()->toOffsetName();
+        
+        return $this->model->query()
+            ->whereRaw(
+                "CONVERT_TZ(STR_TO_DATE(start_date, '%Y-%m-%dT%H:%i:%sZ'), '+00:00','"
+                . $offset
+                . "') BETWEEN '"
+                . $now->startOfWeek()->format("Y-m-d H:i:s")
+                . "' AND '"
+                . $now->endOfWeek()->format("Y-m-d H:i:s")
+                . "'"
+            )
+            ->orderBy("start_date", "asc")
+            ->get();
     }
 }
--- addons/fitlytics/finnito/fitlytics-module/src/Activity/Contract/ActivityRepositoryInterface.php
+++ addons/fitlytics/finnito/fitlytics-module/src/Activity/Contract/ActivityRepositoryInterface.php
@@ -4,5 +4,8 @@ use Anomaly\Streams\Platform\Entry\Contract\EntryRepositoryInterface;
 
 interface ActivityRepositoryInterface extends EntryRepositoryInterface
 {
-
+    public function currentWeekStatistics();
+    public function currentWeekStatisticsByType();
+    public function weekBoundaries();
+    public function thisWeek();
 }
