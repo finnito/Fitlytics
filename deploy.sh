e7a911af5589fa8a0d39d3794021379b73aa323c
Finn Le Sueur <finn@lesueur.nz>
2021-05-17 17:12:11 +1200

    Fix user permissions on deploy

--- deploy.sh
+++ deploy.sh
@@ -5,11 +5,11 @@ cd /srv/fitlytics.lesueur.nz/
 sudo git pull origin main
 sudo -u www-data composer install --profile
 
-php artisan migrate --path=vendor/anomaly/streams-platform/migrations/application
-php artisan migrate --all-addons
-php artisan assets:clear
-php artisan view:clear
-php artisan httpcache:clear
+sudo -u www-data php artisan migrate --path=vendor/anomaly/streams-platform/migrations/application
+sudo -u www-data php artisan migrate --all-addons
+sudo -u www-data php artisan assets:clear
+sudo -u www-data php artisan view:clear
+sudo -u www-data php artisan httpcache:clear
 
 sudo chown -R www-data:www-data ./
 sudo chmod -R ug+rwx storage bootstrap/cache;
