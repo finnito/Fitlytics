eb61f76fcde72c19a0656318187d766e7f5ba2d8
Finn Le Sueur <finn@lesueur.nz>
2021-05-17 17:06:48 +1200

    Updates current week chart

--- addons/fitlytics/finnito/fitlytics-module/src/Http/Controller/APIController.php
+++ addons/fitlytics/finnito/fitlytics-module/src/Http/Controller/APIController.php
@@ -19,4 +19,80 @@ class APIController extends PublicController
             $this->week_of = "now";
         }
     }
+
+    public function currentWeekChart(ActivityRepository $activities)
+    {
+        $out = [];
+        $out["datasets"] = [];
+        $date = \Carbon\CarbonImmutable::parse($this->week_of)->timezone("Pacific/Auckland");
+
+        $types = [
+            ["Run", "#2ecc71"],
+            ["Ride", "#f1c40f"],
+            ["Kayaking", "#3498db"],
+            ["Swim", "#9b59b6"]
+        ];
+
+        foreach ($types as $type) {
+            $week = [
+                ["y" => $activities->newQuery()
+                    ->whereDate("activity_json->start_date_local", $date->startOfWeek()->toDateString())
+                    ->where("type", $type[0])
+                    ->sum("distance"),
+                    "x" => "Mon",
+                ],
+                ["y" => $activities->newQuery()
+                    ->whereDate("activity_json->start_date_local", $date->startOfWeek()->add(1, "day")->toDateString())
+                    ->where("type", $type[0])
+                    ->sum("distance"),
+                "x" => "Tues",
+                ],
+                ["y" => $activities->newQuery()
+                    ->whereDate("activity_json->start_date_local", $date->startOfWeek()->add(2, "day")->toDateString())
+                    ->where("type", $type[0])
+                    ->sum("distance"),
+                    "x" => "Wed",
+                ],
+                ["y" => $activities->newQuery()
+                    ->whereDate("activity_json->start_date_local", $date->startOfWeek()->add(3, "day")->toDateString())
+                    ->where("type", $type[0])
+                    ->sum("distance"),
+                    "x" => "Thur",
+                ],
+                ["y" => $activities->newQuery()
+                    ->whereDate("activity_json->start_date_local", $date->startOfWeek()->add(4, "day")->toDateString())
+                    ->where("type", $type[0])
+                    ->sum("distance"),
+                    "x" => "Fri",
+                ],
+                ["y" => $activities->newQuery()
+                    ->whereDate("activity_json->start_date_local", $date->startOfWeek()->add(5, "day")->toDateString())
+                    ->where("type", $type[0])
+                    ->sum("distance"),
+                    "x" => "Sat",
+                ],
+                ["y" => $activities->newQuery()
+                    ->whereDate("activity_json->start_date_local", $date->startOfWeek()->add(6, "day")->toDateString())
+                    ->where("type", $type[0])
+                    ->sum("distance"),
+                    "x" => "Sun",
+                ],
+            ];
+
+            array_push($out["datasets"], [
+                "data" => $week,
+                "backgroundColor" => $type[1],
+                "label" => $type[0],
+            ]);
+        }
+
+        return json_encode($out);
+
+        // return view(
+        //     'finnito.module.fitlytics::asyncHtml/currentWeekChart',
+        //     [
+        //         "data" => $out,
+        //     ]
+        // );
+    }
 }
