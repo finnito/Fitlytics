2571970dc57c9162e33f11a880adaa9572051d32
Finn Le Sueur <finn@lesueur.nz>
2021-05-19 12:21:08 +1200

    Feat: Improve current week chart
    Only show activity types that occurred. Make use of second axis and hide when not in use.

--- addons/fitlytics/finnito/fitlytics-module/src/Http/Controller/APIController.php
+++ addons/fitlytics/finnito/fitlytics-module/src/Http/Controller/APIController.php
@@ -26,64 +26,98 @@ class APIController extends PublicController
         $out["datasets"] = [];
         $date = \Carbon\CarbonImmutable::parse($this->week_of)->timezone("Pacific/Auckland");
 
-        $types = [
-            ["Run", "#2ecc71"],
-            ["Ride", "#f1c40f"],
-            ["Kayaking", "#3498db"],
-            ["Swim", "#9b59b6"]
+        $types = $activities->newQuery()
+            ->select("type")
+            ->whereBetween("activity_json->start_date_local", [$date->startOfWeek()->toDateString(), $date->endOfWeek()->toDateString()])
+            ->groupBy("type")
+            ->get()
+            ->pluck("type")
+            ->values();
+
+        $colours = [
+            "#fad390",
+            "#6a89cc",
+            "#82ccdd",
+            "#b8e994",
+            "#ff7f50",
+            "#ff6b81",
+            "#9b59b6",
         ];
 
-        foreach ($types as $type) {
+        foreach ($types as $num => $type) {
+            if ($type == "Yoga") {
+                $sumField = "moving_time";
+            } else {
+                $sumField = "distance";
+            }
+
             $week = [
                 ["y" => $activities->newQuery()
                     ->whereDate("activity_json->start_date_local", $date->startOfWeek()->toDateString())
-                    ->where("type", $type[0])
-                    ->sum("distance"),
+                    ->where("type", $type)
+                    ->sum($sumField),
                     "x" => "Mon",
                 ],
                 ["y" => $activities->newQuery()
                     ->whereDate("activity_json->start_date_local", $date->startOfWeek()->add(1, "day")->toDateString())
-                    ->where("type", $type[0])
-                    ->sum("distance"),
+                    ->where("type", $type)
+                    ->sum($sumField),
                 "x" => "Tues",
                 ],
                 ["y" => $activities->newQuery()
                     ->whereDate("activity_json->start_date_local", $date->startOfWeek()->add(2, "day")->toDateString())
-                    ->where("type", $type[0])
-                    ->sum("distance"),
+                    ->where("type", $type)
+                    ->sum($sumField),
                     "x" => "Wed",
                 ],
                 ["y" => $activities->newQuery()
                     ->whereDate("activity_json->start_date_local", $date->startOfWeek()->add(3, "day")->toDateString())
-                    ->where("type", $type[0])
-                    ->sum("distance"),
+                    ->where("type", $type)
+                    ->sum($sumField),
                     "x" => "Thur",
                 ],
                 ["y" => $activities->newQuery()
                     ->whereDate("activity_json->start_date_local", $date->startOfWeek()->add(4, "day")->toDateString())
-                    ->where("type", $type[0])
-                    ->sum("distance"),
+                    ->where("type", $type)
+                    ->sum($sumField),
                     "x" => "Fri",
                 ],
                 ["y" => $activities->newQuery()
                     ->whereDate("activity_json->start_date_local", $date->startOfWeek()->add(5, "day")->toDateString())
-                    ->where("type", $type[0])
-                    ->sum("distance"),
+                    ->where("type", $type)
+                    ->sum($sumField),
                     "x" => "Sat",
                 ],
                 ["y" => $activities->newQuery()
                     ->whereDate("activity_json->start_date_local", $date->startOfWeek()->add(6, "day")->toDateString())
-                    ->where("type", $type[0])
-                    ->sum("distance"),
+                    ->where("type", $type)
+                    ->sum($sumField),
                     "x" => "Sun",
                 ],
             ];
 
-            array_push($out["datasets"], [
-                "data" => $week,
-                "backgroundColor" => $type[1],
-                "label" => $type[0],
-            ]);
+            if ($type == "Yoga") {
+                foreach ($week as $i => $day) {
+                    $week[$i]["y"] = round(floatval($day["y"])/60, 2);
+                }
+                array_push($out["datasets"], [
+                    "data" => $week,
+                    "backgroundColor" => $colours[$num],
+                    "label" => $type,
+                    "yAxisID" => "y2",
+                ]);
+            } else {
+                foreach ($week as $i => $day) {
+                    $week[$i]["y"] = round(floatval($day["y"])/1000, 2);
+                }
+                array_push($out["datasets"], [
+                    "data" => $week,
+                    "backgroundColor" => $colours[$num],
+                    "label" => $type,
+                    "yAxisID" => "y",
+                ]);
+            }
+            
         }
 
         return json_encode($out);
