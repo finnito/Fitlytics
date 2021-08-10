f8729b2cc089fceca29d647bda325d892c9b9880
Finn Le Sueur <finn@lesueur.nz>
2021-08-10 19:13:51 +1200

    Update webpack command

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,3 +1,5 @@
 let mix = require('laravel-mix');
-// mix.css('addons/fitlytics/finnito/fitlytics-theme/resources/css/style.css', 'addons/fitlytics/finnito/fitlytics-theme/resources/css/');
-mix.postCss('addons/fitlytics/finnito/fitlytics-theme/resources/css/style.css', 'public/css/style.css');
+
+mix.postCss(
+    "addons/fitlytics/finnito/fitlytics-theme/resources/css/style.css",
+    'public/css/style.css');
