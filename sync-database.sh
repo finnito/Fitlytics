b4c3eb160b54932b6af178061647b892f5c596e6
Finn Le Sueur <finn@lesueur.nz>
2022-01-20 08:37:08 +1300

    Simplify script

--- sync-database.sh
+++ sync-database.sh
@@ -1,25 +1,8 @@
 #!/usr/bin/env bash
 
-/usr/local/bin/terminal-notifier \
-        -group "com.finnlesueur.fitlytics" \
-        -title "Backup" \
-        -message "Syncing fitlytics.sqlite" \
-        -appIcon "https://seeklogo.com/images/L/linode-logo-0B22204438-seeklogo.com.png"
+# To sync in the other direction
+# rsync -av /Users/finnlesueur/Git/fitlytics/database/fitlytics.sqlite root@172.105.169.195:/srv/fitlytics.lesueur.nz/database/fitlytics.sqlite
 
+cd /Users/finnlesueur/Git/fitlytics || exit
 rsync -av root@172.105.169.195:/srv/fitlytics.lesueur.nz/database/fitlytics.sqlite /Users/finnlesueur/Git/fitlytics/database/
-
-if [ $? -eq 0 ]
-then
-        /usr/local/bin/terminal-notifier \
-                -group "com.finnlesueur.fitlytics" \
-                -title "Backup" \
-                -message "Synced fitlytics.sqlite" \
-                -appIcon "https://seeklogo.com/images/L/linode-logo-0B22204438-seeklogo.com.png"
-else
-        /usr/local/bin/terminal-notifier \
-                -group "com.finnlesueur.fitlytics" \
-                -title "Backup" \
-                -message "Synce Failed fitlytics.sqlite" \
-                -appIcon "https://seeklogo.com/images/L/linode-logo-0B22204438-seeklogo.com.png"
-        exit 1
-fi
+php artisan streams:compile
