a6809a12fe8e020f5bd5a70320d5a0e37b6d41e9
Finn Le Sueur <finn@lesueur.nz>
2022-01-20 08:36:23 +1300

    Retry failed jobs on deploy

--- deploy.sh
+++ deploy.sh
@@ -3,15 +3,21 @@
 cd /srv/fitlytics.lesueur.nz/
 
 sudo git pull origin main
-sudo -u www-data composer install --profile
 
+# Do install things and make sure
+# the cache doesn't get in the way.
+sudo -u www-data composer install --profile
 sudo -u www-data php artisan migrate --path=vendor/anomaly/streams-platform/migrations/application
 sudo -u www-data php artisan migrate --all-addons
 sudo -u www-data php artisan assets:clear
 sudo -u www-data php artisan view:clear
 sudo -u www-data php artisan httpcache:clear
 
+# Make sure permissions are all good
 sudo chown -R www-data:www-data ./
 sudo chmod -R ug+rwx storage bootstrap/cache;
 sudo -u www-data composer dump-autoload --profile
+
+# Restart and retry the webhook queue
 sudo supervisorctl restart fitlytics-queue
+sudo -u www-data php artisan queue:retry all
