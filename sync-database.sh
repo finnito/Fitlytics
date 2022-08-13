9a80a031e5b8611ccc86d9cc8f08b98d6560edc1
Finn Le Sueur <finn@lesueur.nz>
2022-08-13 12:22:27 +1200

    Improve database sync script

--- sync-database.sh
+++ sync-database.sh
@@ -1,8 +1,13 @@
-#!/usr/bin/env bash
+#!/usr/bin/env zsh
 
 # To sync in the other direction
 # rsync -av /Users/finnlesueur/Git/fitlytics/database/fitlytics.sqlite root@172.105.169.195:/srv/fitlytics.lesueur.nz/database/fitlytics.sqlite
 
 cd /Users/finnlesueur/Git/fitlytics || exit
-rsync -av root@172.105.169.195:/srv/fitlytics.lesueur.nz/database/fitlytics.sqlite /Users/finnlesueur/Git/fitlytics/database/
+
+rsync \
+	--archive \
+	root@172.105.169.195:/srv/fitlytics.lesueur.nz/database/fitlytics.sqlite \
+	/Users/finnlesueur/Git/fitlytics/database/
+
 php artisan streams:compile
